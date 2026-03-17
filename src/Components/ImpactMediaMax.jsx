2da0062147eaad1c3cd28cd9ac061a1a937728a6
Praveen Sharma <pshar787898@gmail.com>
2026-03-17 10:54:34 +0530

    Add Her campaign in Impact Mediamax

--- src/Components/ImpactMediaMax.jsx
+++ src/Components/ImpactMediaMax.jsx
@@ -68,6 +68,10 @@ const Campaigns = [
     name: "Hims, Inc.",
     Id: 2700,
   },
+  {
+    name: "Hers, Inc.",
+    Id: 2699,
+  },
 
   {
     // name: "Hims, Inc.",
@@ -410,6 +414,21 @@ export default function ImpactMediaMax() {
         publisher_id: row["Sub Id 2"],
         status: row["Sub Id 2"] === "77" ? "Pending" : "Approved",
         sub1: row["Sub Id 1"],
+        device_id: row["Device Type"] || "unknown",
+      };
+    } else if (campaign.Id === 2699 && campaign.name === "Hers, Inc.") {
+      return {
+        p1: row["Sub Id 1"],
+        created: row["Action Date"],
+        txn_id: row["Action Id"],
+        sale_amount: row["Sale Amount"],
+        revenue: actionEarning,
+        payout: ((actionEarning * 80) / 100).toFixed(10),
+        payout_currency: "USD",
+        campaign_id: campaign.Id,
+        publisher_id: row["Sub Id 2"],
+        status: row["Sub Id 2"] === "77" ? "Pending" : "Approved",
+        sub1: row["Sub Id 3"],
         device_id: row["Device Type"] || "unknown",
       };
     }
