10fb454aaaabe7afe7c2241c686704cc4dd4c6a0
Praveen Sharma <pshar787898@gmail.com>
2026-09-07 12:18:38 +0530

    Update Impact media

--- src/Components/ImpactMediaMax.jsx
+++ src/Components/ImpactMediaMax.jsx
@@ -64,6 +64,10 @@ const Campaigns = [
     name: "NordVPN",
     Id: 3,
   },
+  {
+    name: "LATAM Airlines ( USA/EU )",
+    Id: 2355,
+  },
 ];
 
 export default function ImpactMediaMax() {
@@ -295,6 +299,24 @@ export default function ImpactMediaMax() {
         device_id: row["Device Type"] || "unknown",
       };
     } else if (campaign.Id === 3 && campaign.name === "NordVPN") {
+      return {
+        p1: row["Sub Id 3"],
+        created: row["Action Date"],
+        txn_id: row["Action Id"],
+        sale_amount: row["Sale Amount"],
+        revenue: actionEarning,
+        payout: ((actionEarning * 80) / 100).toFixed(10),
+        payout_currency: "USD",
+        campaign_id: campaign.Id,
+        publisher_id: row["Sub Id 2"],
+        status: row["Sub Id 2"] === "77" ? "Pending" : "Approved",
+        sub1: row["Sub Id 1"],
+        device_id: row["Device Type"] || "unknown",
+      };
+    } else if (
+      campaign.Id === 2355 &&
+      campaign.name === "LATAM Airlines ( USA/EU )"
+    ) {
       return {
         p1: row["Sub Id 3"],
         created: row["Action Date"],
